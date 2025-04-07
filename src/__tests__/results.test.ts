7557cccca3d1833b58cbd6da5dfaa06faa1c933a
1a35e1 <1a35e1@protonmail.com>
2025-04-07 20:23:08 +0100

    feat: Sanity

---
+++ src/__tests__/results.test.ts
@@ -0,0 +1,116 @@
+import { cleanVotes } from "../scoring/utils";
+import { orderChoices } from "../scoring/utils/order-choices";
+import type { Project } from "../types";
+import { generateVotes } from "./utils";
+
+const manifest = {
+	version: "0.2.0",
+	scoring: {
+		algorithm: "copeland",
+		tiebreaker: "average-support",
+		omitBelowChoice: "None Below",
+		groupBy: "group",
+	},
+	data: [
+		{
+			choice: "None Below",
+		},
+		{
+			choice: "A (Basic)",
+			group: "vendorA",
+			label: "Basic Scope for 400k USD",
+		},
+		{
+			choice: "A (Extended)",
+			group: "vendorA",
+			label: "Extended Scope for 800k USD",
+		},
+		{
+			choice: "B (Basic)",
+			group: "vendorB",
+			label: "Basic Scope for 400k USD",
+		},
+		{
+			choice: "C (Extended)",
+			group: "vendorC",
+			label: "Extended Scope for 800k USD",
+		},
+	],
+};
+
+// Reorder the choices to simulate how they might be input randomly in a snapshot vote
+const snapshotChoices = [
+	"C (Extended)",
+	"B (Basic)",
+	"None Below",
+	"A (Basic)",
+	"A (Extended)",
+];
+
+const votes = [
+	{
+		choice: [0, 1, 2, 3, 4],
+		votingPower: 100,
+		voter: "0x1",
+	},
+	{
+		choice: [4, 3, 2, 1, 0],
+		votingPower: 100,
+		voter: "0x2",
+	},
+];
+
+describe("results", () => {
+	let orderedChoices: Project[];
+
+	it("it matches the order of the snapshot choices", () => {
+		// Order our manifest based on how they were input in Snapshot.
+		orderedChoices = orderChoices(manifest.data as Project[], snapshotChoices);
+
+		expect(orderedChoices).toEqual([
+			{
+				choice: "C (Extended)",
+				group: "vendorC",
+				label: "Extended Scope for 800k USD",
+			},
+			{
+				choice: "B (Basic)",
+				group: "vendorB",
+				label: "Basic Scope for 400k USD",
+			},
+			{
+				choice: "None Below",
+			},
+			{
+				choice: "A (Basic)",
+				group: "vendorA",
+				label: "Basic Scope for 400k USD",
+			},
+			{
+				choice: "A (Extended)",
+				group: "vendorA",
+				label: "Extended Scope for 800k USD",
+			},
+		]);
+	});
+
+	it("omits votes below the 'None Below' choice as expected", () => {
+		// Find the index of the "None Below" choice
+		const notBelowIndex = orderedChoices.findIndex(
+			(choice) => choice.choice === manifest.scoring.omitBelowChoice,
+		);
+		const cleanedVotes = cleanVotes(votes, notBelowIndex);
+		expect(cleanedVotes).toEqual([
+			{
+				choice: [0, 1],
+				voter: "0x1",
+				votingPower: 100,
+			},
+			{
+				choice: [4, 3],
+				voter: "0x2",
+				votingPower: 100,
+			},
+		]);
+	});
+});
