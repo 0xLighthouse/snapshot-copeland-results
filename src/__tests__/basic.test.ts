5bf4c9a713fe1b8ccb95ab17aac23cd8bc06ce9d
James MacWhyte <5316685+jmacwhyte@users.noreply.github.com>
2025-04-08 23:05:10 +0100

    fix: Vote reordering tested

--- src/__tests__/basic.test.ts
+++ src/__tests__/basic.test.ts
@@ -1,4 +1,5 @@
-import { copelandWeighted } from '../scoring'
+
+import { copelandWeighted, reorderVotesByGroup } from "../scoring";
 import type { Project, ScoringOptions } from '../types'
 
 const manifest = {
@@ -60,11 +61,82 @@ describe('results', () => {
       params,
     )
 
-    expect(results.map((r) => orderedChoices[r.key].choice)).toEqual([
-      'B (Basic)',
-      'A (Basic)',
-      'C (Basic)',
-      'None Below',
-    ])
-  })
-})
+		expect(results.map((r) => orderedChoices[r.key].choice)).toEqual([
+			"B (Basic)",
+			"A (Basic)",
+			"C (Basic)",
+			"None Below",
+		]);
+	});
+});
+
+const manifest2 = {
+	version: "0.2.0",
+	scoring: {
+		algorithm: "copeland",
+		tiebreaker: "average-support",
+		omitBelowChoice: "None Below",
+		groupBy: "group",
+	},
+	data: [
+		{
+			choice: "A (Basic)",
+			group: "vendorA",
+			label: "Basic Scope for 300k USD",
+		},
+		{
+			choice: "A (Extended)",
+			group: "vendorA",
+			label: "Basic Scope for 300k USD",
+		},
+		{
+			choice: "B (Basic)",
+			group: "vendorB",
+			label: "Basic Scope for 300k USD",
+		},
+		{
+			choice: "C (Basic)",
+			group: "vendorC",
+			label: "Basic Scope for 300k USD",
+		},
+		{
+			choice: "C (Extended)",
+			group: "vendorC",
+			label: "Basic Scope for 300k USD",
+		},
+		{
+			choice: "D (Basic)",
+			group: "vendorD",
+			label: "Basic Scope for 300k USD",
+		},
+	],
+};
+
+const votes2 = [
+	{
+		// The A's and the C's are not together. 
+		// A extended and C basic each come first.
+		choice: [1, 2, 3, 5, 0, 4], 
+		votingPower: 100_000,
+		voter: "0x1",
+	},
+	{
+		choice: [2, 3, 1, 4, 5, 0],
+		votingPower: 100_000,
+		voter: "0x2",
+	},
+];
+
+describe("results", () => {
+	it("reorders votes as expected", () => {
+		const results = reorderVotesByGroup(
+			manifest2.data as Project[],
+			votes2,
+		);
+
+		expect(results.map((r) => r.choice)).toEqual([
+			[1, 0, 2, 3, 4, 5], // A's and C's together, A extended and C basic are still first.
+			[2, 3, 4, 1, 0, 5],
+		]);
+	});
+});
