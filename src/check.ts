4f3584f9536185da175484e4ba6015dbf4c424d1
1a35e1 <1a35e1@protonmail.com>
2025-04-09 23:53:29 +0100

    feat: Example

--- src/check.ts
+++ src/check.ts
@@ -1,10 +1,7 @@
 import { copelandWeighted } from './scoring'
 import { calculateDiff } from './scoring/calculate-diff'
-import { copeland } from './scoring/copeland'
 import { displayResults } from './scoring/display-results'
-import { displayResultsWithDiff } from './scoring/display-results-with-diff'
 import { fetchProposalMetadata, fetchProposalVotes } from './snapshot/snapshot'
-import { ScoringOptions } from './types'
 
 // FIXME: Does not work, as no manifest was provided
 // Netto => https://snapshot.box/#/s:spp-test.eth/proposal/0x14bc28d4202213b2f735be22be131aa5f3b2e790433b8ef232207d0c0ace81e8
@@ -16,12 +13,15 @@ const ID = '0xf26d14c68c589af5de02fbbf67e37d0b5ec5a7795e17b0cf5d2b4ff9ff828424'
 const IS_TESTNET = true
 
 const check = async () => {
-  // Fetch proposal metadata from snapshot
+  /**
+   * 1. Fetch proposal metadata from snapshot
+   */
   const { choices, manifest } = await fetchProposalMetadata({
     proposalId: ID,
     isTestnet: IS_TESTNET,
   })
 
+  // If no manifest is found, render some error message
   if (!manifest) {
     throw new Error('No manifest found')
   }
@@ -30,20 +30,31 @@ const check = async () => {
   console.log(choices, manifest)
 
   console.log('--- VOTES ---')
+  /**
+   * 2. Fetch proposal votes from snapshot
+   */
   const votes = await fetchProposalVotes({
     proposalId: ID,
     isTestnet: IS_TESTNET,
   })
   console.log(votes)
 
+  /**
+   * 3. Calculate results
+   */
   const { results, orderedChoices } = copelandWeighted(manifest, choices, votes)
   console.log(results)
 
+  /**
+   * 4. Display results
+   */
   console.log('--- DISPLAY RESULTS ---')
   console.log(displayResults(results, orderedChoices, manifest.scoring))
 
+  /**
+   * 5. Example of how to show impact of new votes on an existing set of results
+   */
   const baseResults = results
-
   console.log('--- DISPLAY RESULTS WITH DIFF ---')
   const newVotes = [
     ...votes,
