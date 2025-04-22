99753d248883a6e04772d343ddab0463d8863f0f
James MacWhyte <5316685+jmacwhyte@users.noreply.github.com>
2025-04-22 13:51:54 +0100

    feat: Standard copeland omit unrankedFrom w/ tests

--- src/scoring/pipeline/omit-choices-below.ts
+++ src/scoring/pipeline/omit-choices-below.ts
@@ -1,19 +1,33 @@
 import type { Ballot, KeyedChoices } from '../../types'
 
-export const omitFromKey = (votes: Ballot[], fromKey: number) => {
+// Iterates through all ballots, and deletes all choices that appear after "unrankedFrom", so they will not be used in the comparisons.
+// If "keepUnrankedFrom" is true, the "unrankedFrom" choice will be left in the ballots.
+export const omitFromKey = (
+  votes: Ballot[],
+  fromKey: number,
+  keepUnrankedFrom = false,
+) => {
   return votes.map((vote) => {
     // Find the index of the choice from which selections should be unranked
     for (const [index, choice] of vote.choice.entries()) {
       if (choice === fromKey) {
         return {
           ...vote,
-          choice: vote.choice.slice(0, index),
+          choice: vote.choice.slice(0, index + (keepUnrankedFrom ? 1 : 0)),
         }
       }
     }
     return vote
   })
 }
+
+// Remove the specified choice from the keyed choices
+export const omitChoiceByKey = (choices: KeyedChoices, key: number) => {
+  return Object.fromEntries(
+    Object.entries(choices).filter(([k]) => Number(k) !== key),
+  )
+}
+
 // Find which key represents the choice from which selections should be unranked
 export const findUnrankedMarkerKey = (
   orderedChoices: KeyedChoices,
